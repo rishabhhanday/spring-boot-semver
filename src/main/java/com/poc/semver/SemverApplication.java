81ce485d66ca1a2de6daf5a4a4767685c1286833
Rishabh Handay <rishabhhanday.rh@gmail.com>
2020-08-13 15:07:39 +0530

    init commit

---
+++ src/main/java/com/poc/semver/SemverApplication.java
@@ -0,0 +1,13 @@
+package com.poc.semver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemverApplication.class, args);
+	}
+
+}
